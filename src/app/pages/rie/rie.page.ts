8a60a32690609dc3431e4c03f30c380c621c45d8
Kaycee <katrinacarladiaz@gmail.com>
2025-10-03 20:58:45 +0800

    adding rie

--- src/app/pages/rie/rie.page.ts
+++ src/app/pages/rie/rie.page.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-
-@Component({
-  selector: 'app-rie',
-  templateUrl: './rie.page.html',
-  styleUrls: ['./rie.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
-})
-export class RiePage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+import { Component } from '@angular/core'; 
+import { IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonToolbar, IonTitle, IonButtons, 
+IonMenuButton, IonBackButton, IonCardTitle, IonCardHeader, IonCardContent, IonImg, IonCard } from 
+'@ionic/angular/standalone'; 
+import { addIcons } from 'ionicons'; 
+import { documentTextOutline, peopleOutline, bookOutline, schoolOutline, businessOutline, podiumOutline } 
+from 'ionicons/icons'; 
+ 
+@Component({ 
+  selector: 'app-rie', 
+  templateUrl: './rie.page.html', 
+  styleUrls: ['./rie.page.scss'], 
+  standalone: true, 
+  imports: [IonCard, IonImg, IonCardTitle, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, 
+IonToolbar, IonTitle, IonButtons, IonMenuButton, IonBackButton,IonCardHeader, IonCardTitle, 
+IonCardContent] 
+}) 
+export class RiePage { 
+  icons = { 
+    journal: documentTextOutline, 
+    research: bookOutline, 
+    manual: schoolOutline, 
+    milestone: businessOutline, 
+    cmd: podiumOutline, 
+    extension: peopleOutline 
+  }; 
+ 
+  constructor() { 
+    addIcons(this.icons); 
+  } 
+} 
