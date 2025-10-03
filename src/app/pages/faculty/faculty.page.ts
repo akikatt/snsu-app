c0d9724a5eba89ce78105b2c9f2819713bde983c
Kaycee <katrinacarladiaz@gmail.com>
2025-10-03 20:51:56 +0800

    adding faculty

--- src/app/pages/faculty/faculty.page.ts
+++ src/app/pages/faculty/faculty.page.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-
-@Component({
-  selector: 'app-faculty',
-  templateUrl: './faculty.page.html',
-  styleUrls: ['./faculty.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
-})
-export class FacultyPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+import { Component } from '@angular/core'; 
+import { IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonToolbar, 
+IonTitle, IonButtons, IonMenuButton, IonBackButton, IonImg } from 
+'@ionic/angular/standalone'; 
+import { bookOutline, desktopOutline, schoolOutline, cogOutline, ribbonOutline } 
+from 'ionicons/icons'; 
+ 
+@Component({ 
+  selector: 'app-faculty', 
+  templateUrl: './faculty.page.html', 
+  styleUrls: ['./faculty.page.scss'], 
+  standalone: true, 
+  imports: [IonImg, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, 
+IonToolbar, IonTitle, IonButtons, IonMenuButton, IonBackButton] 
+}) 
+export class FacultyPage { 
+  icons = { 
+    ceit: desktopOutline, 
+    teacherEducation: schoolOutline, 
+    technology: cogOutline, 
+    artsSciences: bookOutline, 
+    graduateStudies: ribbonOutline 
+  }; 
 }
