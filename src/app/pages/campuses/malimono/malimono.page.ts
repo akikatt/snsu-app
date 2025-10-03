7be50e040a0e00aa0e5404f4773969fbced5a2a9
Kaycee <katrinacarladiaz@gmail.com>
2025-10-03 21:18:10 +0800

    adding malimono

--- src/app/pages/campuses/malimono/malimono.page.ts
+++ src/app/pages/campuses/malimono/malimono.page.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-
-@Component({
-  selector: 'app-malimono',
-  templateUrl: './malimono.page.html',
-  styleUrls: ['./malimono.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
-})
-export class MalimonoPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+import { Component, OnInit } from '@angular/core'; 
+import { CommonModule } from '@angular/common'; 
+import { FormsModule } from '@angular/forms'; 
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, 
+IonImg, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonMenuButton } 
+from '@ionic/angular/standalone'; 
+ 
+@Component({ 
+  selector: 'app-malimono', 
+  templateUrl: './malimono.page.html', 
+  styleUrls: ['./malimono.page.scss'], 
+  standalone: true, 
+imports: [IonCardTitle, IonCardContent, IonCardHeader, IonCard, IonImg, 
+IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, 
+CommonModule, FormsModule, IonMenuButton] 
+}) 
+export class MalimonoPage implements OnInit { 
+constructor() { } 
+ngOnInit() { 
+} 
+} 
